6a1d1295683750dfc8c4232d754e270ab0166e77
vishalkumar-eng <vk4502374@gmail.com>
2025-05-18 16:32:06 +0530

    Add files via upload

---
+++ reversArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int arr[6]={1,2,3,8,5,6};
+   int start =0, end=5;
+   while(start<end)
+   {
+    swap( arr[start],arr[end]);
+    start++;
+    end--;
+
+   }
+   for(int i=0 ; i<=5;i++)
+   {
+    cout<<arr[i];
+   }
+
+}
